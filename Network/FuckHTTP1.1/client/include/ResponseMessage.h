b8553cd53410bf5f9fe606545e25151108f382f2
Gitlittlerubbish <1293555247@qq.com>
2021-11-24 23:27:26 +0800

    modify fuckhttp1.1 structure and add some basic codes.

--- Network/FuckHTTP1.1/client/include/ResponseMessage.h
+++ Network/FuckHTTP1.1/client/include/ResponseMessage.h
@@ -1,13 +1,33 @@
 #ifndef __RESPONSEMESSAGE_H__
 #define __RESPONSEMESSAGE_H__
 
+#include <cstdio>
+#include <iostream>
 
 class ResponseMessage{
     
 public:
+    ResponseMessage() = default;
+    ResponseMessage(std::string);
+    
+    std::string get_status_line();
+    std::string get_response_header();
+    unsigned char *get_message_payload();
+    std::string get_http_version();
+    int get_status_code();
+    std::string get_status_code_description();
+    std::unordered_map get_response_header_fields();
 
     
 private:
+    std::string _status_line;
+    std::string _response_header;
+    unsigned char *_message_payload;
+
+    std::string _http_version;
+    int _status_code;
+    std::string _status_code_description;
+    std::unordered_map response_header_fields;
 
 
 };
